bc834b095a582b3ca29155785bdfe3a9cee91215
Avinash <avinash.kumar@metacube.com>
2021-02-27 22:07:50 +0530

    first commit

---
+++ Assignment2/Assignment2a/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Driver code
+ * @author avinash.kumar_metacu
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "173";
+		HexCalc hc = new HexCalc(x);
+		System.out.println(hc.add("ab"));
+		System.out.println(hc.multiply("a1"));
+		System.out.println(hc.compareTo("68"));
+		System.out.println(HexCalc.HexToDec("ab"));
+		System.out.println(HexCalc.DecToHex("19"));
+		System.out.println(HexCalc.isHexNumber("jh"));
+	}
+
+}
