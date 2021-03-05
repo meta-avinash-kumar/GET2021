6cf46ae98e3fd4a5a2ede9e68354d1725b672e90
Avinash <avinash.kumar@metacube.com>
2021-03-05 09:16:23 +0530

    Shape factory: Graphics library

---
+++ Assgnment8/src/StartingPoint.java
@@ -0,0 +1,19 @@
+/**
+ * This class holds the coordinate of shapes.
+ * @author avinash.kumar_metacu
+ *
+ */
+public class StartingPoint {
+	double xCoordinate, yCoordinate;
+
+	/**
+	 * @param xCoordinate
+	 * @param yCoordinate
+	 */
+	public StartingPoint(double xCoordinate, double yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+
+
+}
