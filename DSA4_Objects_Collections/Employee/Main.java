fb1afb42e44dac6ed8a7bafc976032810668b9b9
meta-avinash-kumar <avinash.kumar@metacube.com>
2021-03-22 19:37:20 +0530

    DSA:4 Objects & Collections added

---
+++ DSA4_Objects_Collections/Employee/Main.java
@@ -0,0 +1,38 @@
+/**
+ * @author avinash.kumar_metacu
+ *
+ */
+public class Main {
+	/**
+	 * Main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EmployeeCollection ec = new EmployeeCollection();
+		try{
+		ec.add(new Employee(1, "Avinash", "Samastipur"));
+		ec.add(new Employee(2, "Khusbu", "Nalanda"));
+		ec.add(new Employee(3, "Mukesh", "Samastipur"));
+		ec.add(new Employee(4, "Rahul", "Rosera"));
+		ec.add(new Employee(3, "Krishan", "Jaipur"));
+		ec.add(new Employee(5, "Akash", "Agra"));
+		}
+		catch (IllegalArgumentException e){
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Initial Employee record: ");
+		for (Employee e : ec.empList) {
+			System.out.println(e.toString());
+		}
+		ec.sortNatural();
+		System.out.println("After natural sort: ");
+		for (Employee e : ec.empList) {
+			System.out.println(e.toString());
+		}
+		ec.sortByName();
+		System.out.println("After sorting by name: ");
+		for (Employee e : ec.empList) {
+			System.out.println(e.toString());
+		}
+	}
+}
